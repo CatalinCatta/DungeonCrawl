87fb94217595aee941b724395f3c40cdf849f8ca
unknown <catalincatta@yahoo.com>
2022-11-18 08:29:11 +0200

    skeleton

--- Assets/Source/Actors/Characters/Skeleton.cs
+++ Assets/Source/Actors/Characters/Skeleton.cs
@@ -1,4 +1,8 @@
 ﻿using UnityEngine;
+using System.Collections;
+using DungeonCrawl.Core;
+using DungeonCrawl.Actors.Static;
+using Assets.Source.Core;
 
 namespace DungeonCrawl.Actors.Characters
 {
@@ -6,12 +10,58 @@ namespace DungeonCrawl.Actors.Characters
     {
         public override bool OnCollision(Actor anotherActor)
         {
-            return true;
+            return false;
         }
 
         protected override void OnDeath()
         {
             Debug.Log("Well, I was already dead anyway...");
+        }
+
+        public override void Hit(Actor actor)
+        {
+            if (actor is Player player)
+            {
+                Debug.Log(Position);
+                Debug.Log(Position);
+                if (Position == (6, -8))
+                {
+                    UserInterface.Singleton.SetText("Press SPACE for combat", UserInterface.TextPosition.TopRight);
+                }
+                player.ApplyDamage(Damage);
+            }
+        }
+
+        IEnumerator Start()
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(1f);
+                HitEnemy();
+            }
+        }
+
+        public override void Starter()
+        {
+            MaxHealth = 15;
+            ActualHealth = 15;
+            Damage = 5;
+        }
+
+        public override void Drop()
+        {
+            if (Position == (6, -8))
+            {
+                ActorManager.Singleton.Spawn<Key>(Position);
+            }
+            else
+            {
+                var rand = new System.Random();
+                var dropRate = rand.Next(100);
+                if (dropRate < 20) { ActorManager.Singleton.Spawn<Heal>(Position); }
+                if (dropRate == 20) { ActorManager.Singleton.Spawn<Meat>(Position); }
+            }
+
         }
 
         public override int DefaultSpriteId => 316;
