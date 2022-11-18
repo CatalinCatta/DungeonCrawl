ced2ada329b6ce07d9390de43cb3697d78bb0237
unknown <catalincatta@yahoo.com>
2022-11-18 08:40:57 +0200

    right roof

---
+++ Assets/Source/Actors/Static/RightRoof.cs
@@ -0,0 +1,8 @@
+namespace DungeonCrawl.Actors.Static
+{
+    public class RightRoof : Actor
+    {
+        public override int DefaultSpriteId => 731;
+        public override string DefaultName => "RightRoof";
+    }
+}
