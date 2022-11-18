80b39742f17b20aa7da3fa2305457c2af15e2149
unknown <catalincatta@yahoo.com>
2022-11-18 08:36:40 +0200

    floor2

---
+++ Assets/Source/Actors/Static/Floor2.cs
@@ -0,0 +1,11 @@
+namespace DungeonCrawl.Actors.Static
+{
+    public class Floor2 : Actor
+    {
+        public override int DefaultSpriteId => 16;
+        public override string DefaultName => "Floor2";
+
+        public override bool Detectable => false;
+
+    }
+}
