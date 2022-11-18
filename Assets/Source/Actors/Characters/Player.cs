4ebe1fdf64bbbc5c2d3efc09addea2e68139dc32
unknown <catalincatta@yahoo.com>
2022-11-18 08:29:00 +0200

    player

--- Assets/Source/Actors/Characters/Player.cs
+++ Assets/Source/Actors/Characters/Player.cs
@@ -1,33 +1,118 @@
 ﻿using UnityEngine;
+using Assets.Source.Core;
+using DungeonCrawl.Core;
+using DungeonCrawl.Actors.Static;
+using System.Linq;
 
 namespace DungeonCrawl.Actors.Characters
 {
     public class Player : Character
     {
+        public bool Dog = false;
+        public GameObject inventoryDisplay;
+        public Inventory inventory = new Inventory();
+        public Actor ItemOnGround;
+        public bool SecretShowed = false;
+        public bool InsideSecretArea = false;
+        public Actor companion;
+        public bool Map = false;
+        public bool Boss1Killed = false;
+
+        public override void Starter()
+        {
+            inventoryDisplay = GameObject.Find("Inventory");
+            if (inventoryDisplay != null) { inventoryDisplay.SetActive(false); }
+            MaxHealth = 100;
+            ActualHealth = 100;
+            Damage = 1;
+            ActualArmor = 0;
+            MaxArmor = 0;
+
+            ShowStats();
+            UserInterface.Singleton.SetText("Look, there is a sword, let's pick it up!", UserInterface.TextPosition.TopCenter);
+        }
+
+        public void ShowStats()
+        {
+            UserInterface.Singleton.SetText("HP: " + ActualHealth + " / " + MaxHealth + "\nDMG: " + (companion is DogCompanion ? (Damage * 2) : Damage) + "\nARMOR: " + ActualArmor + " / " + MaxArmor, UserInterface.TextPosition.TopLeft);
+        }
+
         protected override void OnUpdate(float deltaTime)
         {
-            if (Input.GetKeyDown(KeyCode.W))
+            inventory.selectItem();
+
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
             {
                 // Move up
                 TryMove(Direction.Up);
             }
 
-            if (Input.GetKeyDown(KeyCode.S))
+            if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
             {
                 // Move down
                 TryMove(Direction.Down);
             }
 
-            if (Input.GetKeyDown(KeyCode.A))
+            if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
             {
                 // Move left
                 TryMove(Direction.Left);
             }
 
-            if (Input.GetKeyDown(KeyCode.D))
+            if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
             {
                 // Move right
                 TryMove(Direction.Right);
+            }
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                UserInterface.Singleton.SetText("", UserInterface.TextPosition.TopCenter);
+                // pickUp
+                PickUp();
+            }
+
+            if (Input.GetKeyDown(KeyCode.I))
+            {
+                // inventory
+                inventory.ShowInventory(inventoryDisplay);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                // hit
+                HitEnemy();
+            }
+
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                var start = GameObject.Find("Start");
+                if (start != null)
+                {
+                    start.SetActive(false);
+                }
+            }
+        }
+
+        public void PickUp()
+        {
+            UserInterface.Singleton.SetText("", UserInterface.TextPosition.TopRight);
+
+            if (ItemOnGround.OnGround && ItemOnGround != null)
+            {
+                
+                if (ItemOnGround is Sword) { UserInterface.Singleton.SetText("Nice! Now equip it.. Hint: Press I!", UserInterface.TextPosition.TopCenter); }
+                inventory.Add(ItemOnGround);
+                ActorManager.Singleton.DestroyActor(ItemOnGround);
+            }
+
+        }
+
+        public override void Hit(Actor actor)
+        {
+            if (actor is Character enemy)
+            {
+                enemy.ApplyDamage(companion is DogCompanion ? Damage * 2: Damage);
             }
         }
 
@@ -38,10 +123,38 @@ namespace DungeonCrawl.Actors.Characters
 
         protected override void OnDeath()
         {
-            Debug.Log("Oh no, I'm dead!");
+            ActualHealth = 0;
+            ShowStats();
         }
 
         public override int DefaultSpriteId => 24;
         public override string DefaultName => "Player";
+        public override void Drop() { }
+
+        public void SecretSwitch()
+        {
+            SecretShowed = !SecretShowed;
+            Debug.Log("in");
+            foreach (var wall in Resources.FindObjectsOfTypeAll<GameObject>().Where(obj => obj.name.Contains("Hiden")))
+            {
+                var wall2 = wall.GetComponent<Renderer>();
+                Color color = wall2.material.color;
+                color.a = SecretShowed ? 1 : 0;
+                wall2.material.color = color;
+            }
+
+            foreach (var wall in Resources.FindObjectsOfTypeAll<GameObject>().Where(obj => !obj.name.Contains("Hiden")))
+            {
+                var wall2 = wall.GetComponent<Renderer>();
+                if (wall2 != null && wall.name != this.name)
+                {
+                    Color color = wall2.material.color;
+                    color.a = SecretShowed ? (float)0.5 : 1;
+                    wall2.material.color = color;
+                }
+            }
+
+        }
+
     }
 }
