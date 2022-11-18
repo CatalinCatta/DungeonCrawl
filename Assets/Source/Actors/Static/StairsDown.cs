3ebbfe6a2fb0d7058b835abf0a3dace8b074e83f
unknown <catalincatta@yahoo.com>
2022-11-18 08:43:10 +0200

    stairs down

---
+++ Assets/Source/Actors/Static/StairsDown.cs
@@ -0,0 +1,9 @@
+namespace DungeonCrawl.Actors.Static
+{
+    public class StairsDown : Actor
+    {
+        public override int DefaultSpriteId => 290;
+        public override string DefaultName => "StairsDown";
+        public override int Z => -1;
+    }
+}
