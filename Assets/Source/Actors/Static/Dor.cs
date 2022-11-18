0e82b9bc5dc0ce82a11a9c07593c4217922be35b
unknown <catalincatta@yahoo.com>
2022-11-18 08:34:25 +0200

    dor

---
+++ Assets/Source/Actors/Static/Dor.cs
@@ -0,0 +1,8 @@
+namespace DungeonCrawl.Actors.Static
+{
+    public class Dor : Actor
+    {
+        public override int DefaultSpriteId => 441;
+        public override string DefaultName => "Floor";
+    }
+}
