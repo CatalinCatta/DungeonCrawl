d7df5add644f6e87771d8e0197fdbac8c351e516
unknown <catalincatta@yahoo.com>
2022-11-18 08:26:43 +0200

    ghost

--- Assets/Source/Actors/Characters/Character.cs
+++ Assets/Source/Actors/Characters/Character.cs
@@ -1,21 +1,62 @@
 ﻿using DungeonCrawl.Core;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.Source.Core;
+using DungeonCrawl.Actors.Static;
+using System;
 
 namespace DungeonCrawl.Actors.Characters
 {
     public abstract class Character : Actor
     {
-        public int Health { get; private set; }
+        public int MaxHealth { get; set; }
+        public int ActualHealth { get; set; }
+        public int Damage { get; set; }
+        public int MaxArmor { get; set; }
+        public int ActualArmor { get; set; }
 
-        public void ApplyDamage(int damage)
+        public void ApplyDamage(int damage, string curse = null)
         {
-            Health -= damage;
+            if (this.ActualArmor > 0 && curse == null)
+            {
+                this.ActualArmor -= damage/2;
+                
+                if(this is Boss1 b1 && ActualArmor <= 0)
+                {
+                    b1.TrueForm();
+                }
+            }
+            else
+            {
+                this.ActualHealth -= damage;
+            }
 
-            if (Health <= 0)
+            if (this is Player player)
+            {
+                player.ShowStats();
+            }
+
+            if (this is Boss1 boss1)
+            {
+                boss1.ShowStats();
+            }
+
+            if (ActualHealth <= 0)
             {
                 // Die
+                Drop();
                 OnDeath();
 
                 ActorManager.Singleton.DestroyActor(this);
+            }
+        }
+
+        public void Heal(int hp)
+        {
+            ActualHealth += hp;
+            if (ActualHealth > MaxHealth)
+            {
+                ActualHealth = MaxHealth;
             }
         }
 
@@ -24,6 +65,34 @@ namespace DungeonCrawl.Actors.Characters
         /// <summary>
         ///     All characters are drawn "above" floor etc
         /// </summary>
-        public override int Z => -1;
+        public override int Z => -2;
+
+        public abstract void Starter();
+
+        public void HitEnemy()
+        {
+            List<Actor> actors = new List<Actor>
+            {
+                ActorManager.Singleton.GetActorAt((Position.x, Position.y + 1)),
+                ActorManager.Singleton.GetActorAt((Position.x, Position.y - 1)),
+                ActorManager.Singleton.GetActorAt((Position.x - 1, Position.y)),
+                ActorManager.Singleton.GetActorAt((Position.x + 1, Position.y))
+            };
+
+            foreach(Actor actor in actors)
+            {
+                Hit(actor);
+            }
+        }
+
+        public abstract void Hit(Actor actor);
+
+        public virtual void Drop()
+        {
+            var rand = new System.Random();
+            var dropRate = rand.Next(100);
+            if (dropRate < 20) { ActorManager.Singleton.Spawn<Heal>(Position); }
+            if (dropRate == 20) { ActorManager.Singleton.Spawn<Meat>(Position); }
+        }
     }
 }
