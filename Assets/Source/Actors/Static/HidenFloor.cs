31b69bcf59e4f59f608fdf7fa6ceb3e34478112a
unknown <catalincatta@yahoo.com>
2022-11-18 08:37:41 +0200

    hiden floor

---
+++ Assets/Source/Actors/Static/HidenFloor.cs
@@ -0,0 +1,8 @@
+namespace DungeonCrawl.Actors.Static
+{
+    public class HidenFloor : Actor
+    {
+        public override int DefaultSpriteId => 825;
+        public override string DefaultName => "HidenFloor";
+    }
+}
