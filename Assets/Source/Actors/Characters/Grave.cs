705aa7a8413ce3b751d1cc95205137629dc16844
unknown <catalincatta@yahoo.com>
2022-11-18 08:28:29 +0200

    grave

---
+++ Assets/Source/Actors/Characters/Grave.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using System.Collections;
+using DungeonCrawl.Core;
+using DungeonCrawl.Actors.Static;
+using Assets.Source.Core;
+using System;
+
+namespace DungeonCrawl.Actors.Characters
+{
+    public class Grave : Character
+    {
+        public Ghost ghost;
+
+        public override bool OnCollision(Actor anotherActor)
+        {
+            return false;
+        }
+
+        protected override void OnDeath()
+        {
+            if (ghost != null && ghost.ActualHealth > 0) { ghost.ApplyDamage(ghost.MaxHealth); }
+        }
+
+        public override void Drop()
+        {
+
+        }
+
+        public override void Hit(Actor actor)
+        {
+
+        }
+
+        public override void Starter()
+        {
+            MaxHealth = 20;
+            ActualHealth = 20;
+        }
+
+        public override int DefaultSpriteId => 672;
+        public override string DefaultName => "Grave";
+    }
+}
