f0da3a6c4dda0d32d217d869f87ce1e91f0cf154
unknown <catalincatta@yahoo.com>
2022-11-18 08:38:50 +0200

    hiden wall2

---
+++ Assets/Source/Actors/Static/HidenWall2.cs
@@ -0,0 +1,8 @@
+namespace DungeonCrawl.Actors.Static
+{
+    public class HidenWall2 : Actor
+    {
+        public override int DefaultSpriteId => 1050;
+        public override string DefaultName => "HidenWall2";
+    }
+}
